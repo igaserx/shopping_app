ede717bcbc57ec8dd396077261523757abe34524
igaserx <gasseralaa11@gmail.com>
2025-08-31 17:25:54 +0300

    - get it impl.

--- lib/core/DI/di.dart
+++ lib/core/DI/di.dart
@@ -1,5 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shopping_app/features/auth/data/repositories/auth_reop_impl.dart';
+import 'package:shopping_app/features/auth/domain/repositories/auth_repo.dart';
+import 'package:shopping_app/features/auth/domain/usecases/signin_use_case.dart';
+import 'package:shopping_app/features/auth/domain/usecases/signout_use_case.dart';
+import 'package:shopping_app/features/auth/domain/usecases/signup_use_case.dart';
+import 'package:shopping_app/features/auth/presentation/cubits/auth_cubit.dart';
 
-final di  = GetIt.instance;
+final di = GetIt.instance;
 
-Future<void> init() async {}
+Future<void> init() async {
+  // Firebase
+  di.registerLazySingleton(() => FirebaseAuth.instance);
+
+  // Repositories
+  di.registerLazySingleton<AuthRepository>(
+    () => AuthRepositoryImpl(firebaseAuth: di()),
+  );
+
+  // Use Cases
+  di.registerLazySingleton(() => SignInUseCase( di()));
+  di.registerLazySingleton(() => SignUpUseCase( di()));
+  di.registerLazySingleton(() => SignOutUseCase(di()));
+  
+  // Cubits
+  di.registerFactory(
+    () => AuthCubit(
+      signInUseCase: di(),
+      signUpUseCase: di(),
+      signOutUseCase: di(),
+    ),
+  );
+}

30d06f9e9c2c9120b61b17108d0b2e05c8d5e639
igaserx <gasseralaa11@gmail.com>
2025-09-17 22:09:55 +0300

    - product entity.

---
+++ lib/features/products/domain/entities/product_entity.dart
@@ -0,0 +1,17 @@
+class ProductEntity {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  final double rating;
+
+  const ProductEntity({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+    required this.rating,
+  });
+}

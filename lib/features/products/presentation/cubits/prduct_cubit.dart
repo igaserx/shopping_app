232c1fa0b92d5257f3bbe2b53e3431bc3ffaf4ba
igaserx <gasseralaa11@gmail.com>
2025-09-21 06:43:41 +0300

    - getProductsByCategory cubit.

--- lib/features/products/presentation/cubits/prduct_cubit.dart
+++ lib/features/products/presentation/cubits/prduct_cubit.dart
@@ -1,11 +1,13 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopping_app/features/products/domain/usecases/get_all_products_usecase.dart';
+import 'package:shopping_app/features/products/domain/usecases/get_product_by_category_usecase.dart';
 import 'package:shopping_app/features/products/presentation/cubits/prduct_state.dart';
 
 class ProductCubit extends Cubit<ProductState> {
   final GetAllProductsUseCase getAllProductsUseCase;
+  final GetProductByCategoryUsecase getProductByCategoryUsecase;
 
-  ProductCubit({required this.getAllProductsUseCase}) : super(ProductInitial());
+  ProductCubit({required this.getAllProductsUseCase, required this.getProductByCategoryUsecase}) : super(ProductInitial());
 
 getAllProducts() async {
   emit(ProductLoading());
@@ -16,4 +18,13 @@ getAllProducts() async {
     emit(ProductError(message: e.toString()));
   }
 }
+ getProductsByCategory(List<String> category) async {
+    emit(ProductLoading());
+    try {
+      final products = await getProductByCategoryUsecase.call(category);
+      emit(ProductLoaded(products: products));
+    } catch (e) {
+      emit(ProductError(message: e.toString()));
+    }
+  }
 }

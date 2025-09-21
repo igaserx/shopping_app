2a56f5d91f5aa5166d3e121080bc148e181db1c7
igaserx <gasseralaa11@gmail.com>
2025-09-21 06:42:27 +0300

    - ApiKeys &  get product by category fun.

--- lib/core/database/apis/end_points.dart
+++ lib/core/database/apis/end_points.dart
@@ -2,5 +2,14 @@
 class EndPoints 
 {
    static const String baseUrl = "https://dummyjson.com/";
+   static const String product = "products"; 
 }
-class ApiKeys {}
+class ApiKeys {
+  static const String id = "id";
+  static const String title = "title";
+  static const String description = "description";
+  static const String price = "price";
+  static const String imageUrl = "thumbnail";
+  static const String rating = "rating";
+
+}
--- lib/features/products/data/data_sources/data_source.dart
+++ lib/features/products/data/data_sources/data_source.dart
@@ -1,8 +1,8 @@
 import 'package:shopping_app/core/database/apis/api_consumer.dart';
 import 'package:shopping_app/core/database/apis/end_points.dart';
-
 abstract class ProductRemoteDataSource {
   Future<List<Map<String, dynamic>>> getAllProducts();
+  Future<List<Map<String, dynamic>>> getProductByCategory(List<String> categories);
 }
 
 class ProductRemoteDatasourcesImpl extends ProductRemoteDataSource {
@@ -12,8 +12,22 @@ class ProductRemoteDatasourcesImpl extends ProductRemoteDataSource {
 
   @override
   Future<List<Map<String, dynamic>>> getAllProducts() async {
-    final response = await api.get(EndPoints.product); 
+    final response = await api.get("${EndPoints.product}?limit=100"); 
     final products = response['products'] as List<dynamic>;
     return List<Map<String, dynamic>>.from(products);
   }
+  
+  @override
+ Future<List<Map<String, dynamic>>> getProductByCategory(
+    List<String> categories) async {
+  List<Map<String, dynamic>> allProducts = [];
+
+  for (final category in categories) {
+    final response = await api.get("${EndPoints.product}/category/$category");
+    final products = response['products'] as List<dynamic>;
+    allProducts.addAll(List<Map<String, dynamic>>.from(products));
+  }
+
+  return allProducts;
+}
 }

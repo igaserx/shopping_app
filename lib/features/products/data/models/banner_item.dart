c11c79e2a8d8dd4f6e5beeaf85d6dbf07973e29a
igaserx <gasseralaa11@gmail.com>
2025-09-21 03:52:50 +0300

    - banner items.

---
+++ lib/features/products/data/models/banner_item.dart
@@ -0,0 +1,11 @@
+class BannerItem {
+  final String title;
+  final String subtitle;
+  final String imageUrl;
+
+  const BannerItem({
+    required this.title,
+    required this.subtitle,
+    required this.imageUrl,
+  });
+}

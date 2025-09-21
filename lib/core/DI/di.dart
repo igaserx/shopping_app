5a9b8b7cfb45330cf04bccf9da2978eaf697d2d2
igaserx <gasseralaa11@gmail.com>
2025-09-21 17:36:25 +0300

    - di.

--- lib/core/DI/di.dart
+++ lib/core/DI/di.dart
@@ -1,34 +1,68 @@
+import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shopping_app/core/database/apis/api_consumer.dart';
+import 'package:shopping_app/core/database/apis/dio_consumer.dart';
 import 'package:shopping_app/features/auth/data/repositories/auth_reop_impl.dart';
 import 'package:shopping_app/features/auth/domain/repositories/auth_repo.dart';
 import 'package:shopping_app/features/auth/domain/usecases/signin_use_case.dart';
 import 'package:shopping_app/features/auth/domain/usecases/signout_use_case.dart';
 import 'package:shopping_app/features/auth/domain/usecases/signup_use_case.dart';
 import 'package:shopping_app/features/auth/presentation/cubits/auth_cubit.dart';
+import 'package:shopping_app/features/products/data/data_sources/data_source.dart';
+import 'package:shopping_app/features/products/data/repositories/product_repository_impl.dart';
+import 'package:shopping_app/features/products/domain/repositories/products_repo.dart';
+import 'package:shopping_app/features/products/domain/usecases/get_all_products_usecase.dart';
+import 'package:shopping_app/features/products/domain/usecases/get_product_by_category_usecase.dart';
+import 'package:shopping_app/features/products/presentation/cubits/prduct_cubit.dart';
+
 
 final di = GetIt.instance;
 
 Future<void> init() async {
-  // Firebase
-  di.registerLazySingleton(() => FirebaseAuth.instance);
+  //! Core Layer 
+  di.registerLazySingleton<Dio>(() => Dio());
+  di.registerLazySingleton<ApiConsumer>(() => DioConsumer(dio: di()));
+  di.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
 
-  // Repositories
+  //! Features
+  // Auth Feature
+  // Data layer
   di.registerLazySingleton<AuthRepository>(
-    () => AuthRepositoryImpl(firebaseAuth: di()),
+    () => AuthRepositoryImpl(firebaseAuth: di<FirebaseAuth>()),
   );
 
-  // Use Cases
-  di.registerLazySingleton(() => SignInUseCase( di()));
-  di.registerLazySingleton(() => SignUpUseCase( di()));
-  di.registerLazySingleton(() => SignOutUseCase(di()));
+  // Domain layer
+  di.registerLazySingleton(() => SignInUseCase(di<AuthRepository>()));
+  di.registerLazySingleton(() => SignUpUseCase(di<AuthRepository>()));
+  di.registerLazySingleton(() => SignOutUseCase(di<AuthRepository>()));
   
-  // Cubits
+  // Presentation layer
   di.registerFactory(
     () => AuthCubit(
       signInUseCase: di(),
       signUpUseCase: di(),
       signOutUseCase: di(),
+    ),
+  );
+  
+  // Products Feature
+  // Data layer
+  di.registerLazySingleton<ProductRemoteDataSource>(
+    () => ProductRemoteDatasourcesImpl(api: di<ApiConsumer>()),
+  );
+  di.registerLazySingleton<ProductRepository>(
+    () => ProductRepositoryImpl(remoteDataSource: di()),
+  );
+  // Domain layer
+  di.registerLazySingleton(() => GetAllProductsUseCase(repository: di()));
+  di.registerLazySingleton(() => GetProductByCategoryUsecase(repo: di()));
+
+  // Presentation layer
+  di.registerFactory(
+    () => ProductCubit(
+      getAllProductsUseCase: di(),
+      getProductByCategoryUsecase: di(),
     ),
   );
 }

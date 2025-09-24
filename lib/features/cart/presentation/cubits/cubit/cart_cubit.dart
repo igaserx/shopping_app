a4b97926287f5fa9f6ff4a6a148af719e539f87b
igaserx <gasseralaa11@gmail.com>
2025-09-24 21:22:25 +0300

    - cart cubit.

---
+++ lib/features/cart/presentation/cubits/cubit/cart_cubit.dart
@@ -0,0 +1,56 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shopping_app/features/cart/domain/entities/cart_item.dart';
+import 'package:shopping_app/features/cart/presentation/cubits/cubit/cart_state.dart';
+
+
+class CartCubit extends Cubit<CartState> {
+  CartCubit() : super(CartState(items: []));
+
+  void addItem(CartItem item) {
+    final existingIndex = state.items.indexWhere(
+      (element) => element.product.id == item.product.id,
+    );
+
+    if (existingIndex != -1) {
+      final updatedItem = state.items[existingIndex].copyWith(
+        quantity: state.items[existingIndex].quantity + item.quantity,
+      );
+
+      final updatedItems = List<CartItem>.from(state.items)
+        ..[existingIndex] = updatedItem;
+
+      emit(state.copyWith(items: updatedItems));
+    } else {
+      emit(state.copyWith(items: [...state.items, item]));
+    }
+  }
+
+  void removeItem(int productId) {
+    emit(
+      state.copyWith(
+        items: state.items
+            .where((item) => item.product.id != productId)
+            .toList(),
+      ),
+    );
+  }
+
+  void updateQuantity(int productId, int quantity) {
+    final index = state.items.indexWhere((item) => item.product.id == productId);
+    if (index != -1) {
+      if (quantity <= 0) {
+        
+        removeItem( productId);
+      } else {
+        final updatedItem = state.items[index].copyWith(quantity: quantity);
+        final updatedItems = List<CartItem>.from(state.items)
+          ..[index] = updatedItem;
+        emit(state.copyWith(items: updatedItems));
+      }
+    }
+  }
+
+  void clearCart() {
+    emit(state.copyWith(items: []));
+  }
+}
---
+++ lib/features/cart/presentation/cubits/cubit/cart_state.dart
@@ -0,0 +1,14 @@
+import 'package:shopping_app/features/cart/domain/entities/cart_item.dart';
+
+class CartState {
+  final List<CartItem> items;
+
+  CartState({required this.items});
+
+  double get totalPrice =>
+      items.fold(0, (sum, item) => sum + item.totalPrice);
+
+  CartState copyWith({List<CartItem>? items}) {
+    return CartState(items: items ?? this.items);
+  }
+}

aa7c58c8de981b5ce839a71d2a7d73f696d69027
igaserx <gasseralaa11@gmail.com>
2025-09-26 21:29:42 +0300

    - buy, proceeding to buy & change languages.

--- lib/core/utils/utils.dart
+++ lib/core/utils/utils.dart
@@ -1,10 +1,12 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopping_app/core/widgets/custom_snack_bar.dart';
-import 'package:shopping_app/features/cart/presentation/cubits/cubit/cart_cubit.dart';
+import 'package:shopping_app/features/cart/cubits/cart_cubit.dart';
+import 'package:shopping_app/features/cart/models/cart_item.dart';
+import 'package:shopping_app/features/check_out/views/buy_now_view.dart';
+import 'package:shopping_app/features/check_out/views/check_out_view.dart';
 import 'package:shopping_app/features/products/domain/entities/product_entity.dart';
-
-import '../../features/cart/domain/entities/cart_item.dart';
 
 class Utils {
   //! Discount Method
@@ -29,5 +31,33 @@ class Utils {
     context,e.toString(), type: SnackBarType.warning
    );
 }
+}
+
+  //! buy 
+  static void onBuyNow(BuildContext context, {required ProductEntity product}) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) =>  BuyNowView(product: product,),
+      ),
+  );
+    CustomSnackBar.show(context, 'Proceeding to buy ${product.title}', type: SnackBarType.custom,customColor: Colors.amber[800]);
+  }
+  //! Proceeding to buy 
+  static void onProceedingToBuy(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const CheckoutView(),
+      ),
+    );
+    CustomSnackBar.show(context, 'Proceeding to buy all items', type: SnackBarType.custom, customColor:  Colors.amber[800]);
+  }
+  //! change language
+  static void changeToArabic(BuildContext context) {
+  context.setLocale(const Locale('ar'));
+  }
+  static void changeToEnglish(BuildContext context) {
+  context.setLocale(const Locale('en'));
   }
 }
